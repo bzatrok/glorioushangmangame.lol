c1851ce74530f0dddd540f228aa459f1088669fa
Ben Zatrok <ben@amberglass.co>
2025-08-04 16:59:27 +0200

    Add run script and configure port 8090

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Starting Pete's Beer Hangman Game..."
+echo "The game will be available at http://localhost:8090"
+echo "Press Ctrl+C to stop the server"
+echo ""
+
+mvn clean compile jetty:run
